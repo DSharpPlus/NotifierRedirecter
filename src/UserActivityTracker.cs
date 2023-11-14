aad9eeb00fc6dc51c16b1b364953f014e38370be
Lunar Starstrum <lunar@forsaken-borders.net>
2023-11-14 14:05:25 -0600

    Prune inactive users from the activity tracker

--- src/UserActivityTracker.cs
+++ src/UserActivityTracker.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Concurrent;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace NotifierRedirecter;
@@ -7,11 +8,14 @@ namespace NotifierRedirecter;
 public sealed class UserActivityTracker
 {
     private static readonly TimeSpan _activityTimeout = TimeSpan.FromSeconds(5);
+    private static readonly TimeSpan _cleanupInterval = TimeSpan.FromMinutes(5);
 
     /// <summary>
     /// Tracks the user's last activity within a channel.
     /// </summary>
     private readonly ConcurrentDictionary<ulong, (ulong ChannelId, DateTimeOffset LastActivity)> _tracker = new();
+
+    public UserActivityTracker() => _ = CleanupInactiveUsersAsync();
 
     public void UpdateUser(ulong userId, ulong channelId) => this._tracker.AddOrUpdate(userId, (channelId, DateTimeOffset.UtcNow), (_, _) => (channelId, DateTimeOffset.UtcNow));
     public async ValueTask<bool> IsActiveAsync(ulong userId, ulong channelId)
@@ -20,5 +24,21 @@ public sealed class UserActivityTracker
         // If they do, the _tracker will be updated and we can avoid a DM.
         await Task.Delay(_activityTimeout);
         return this._tracker.TryGetValue(userId, out (ulong ChannelId, DateTimeOffset LastActivity) value) && value.ChannelId == channelId && value.LastActivity > DateTimeOffset.UtcNow.AddSeconds(-15);
+    }
+
+    public async Task CleanupInactiveUsersAsync()
+    {
+        PeriodicTimer timer = new(_cleanupInterval);
+        while (await timer.WaitForNextTickAsync())
+        {
+            foreach ((ulong userId, (ulong _, DateTimeOffset LastActivity)) in this._tracker)
+            {
+                // If the user hasn't done anything since the last cleanup interval, remove them from the tracker.
+                if (LastActivity < DateTimeOffset.UtcNow.Add(-_cleanupInterval))
+                {
+                    this._tracker.TryRemove(userId, out _);
+                }
+            }
+        }
     }
 }
